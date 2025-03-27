0ce2c45339cfc9b9b55f876bcc22301a03892cdf
Yihengggoh <s3882810@student.rmit.edu.au>
2025-03-27 16:21:09 +1100

    new: skeleton code for editing dog profile

---
+++ edit_dog.php
@@ -0,0 +1,57 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Edit dog profile</title>
+    </head>
+    <body>
+        <h3>Edit Dog Profile</h3> <!--add link to go back to previous page 'settings' page-->
+        <p>Robbie + Add Dog</p> <!--get dog name from DB/backend--><!-- link to dog profile creation(clarify) -->
+
+        <p>Name</p>
+        <p>Display dog name + edit functionality & icon</p><br> <!--form(?) to send new name to db + input validation-->
+
+        <p>Choose Your profile Picture</p>
+        <p>upload icon + current dog picture</p> <br><!-- get image from db + form to upload and send new image to db-->
+
+        <p>Breed</p>
+        <p>Display dog breed + edit functionality & icon</p><br><!-- get breed from db + form to send new breed to db + input validation-->
+
+        <p>Gender</p>
+        <p>Selection between male/female</p><br><!-- form to get male/female button using radio?-->
+
+        <p>Date of Birth</p>
+        <p>Cake icon + DoB in (mmm dd, yyyy) + edit functionality & icon</p><br><!-- clarify what client would like (user input/clicking on visual calender etc) + input validation-->
+
+        <p>Weight</p>
+        <p>Weight in kg (9.0kg) + edit functionality & icon</p><br><!-- get weight from db + form to send new weight to db + input validation of correct weight format-->
+
+        <p>Microchip Number</p>
+        <p>Display microchip number + edit functionality & icon</p><br><!-- get microchip number + form to send new microchip number to db + input validation of correct number format-->
+
+        <p>Registration Number</p>
+        <p>Display Registration Number + edit functionality & icon</p><br><!-- get registration number + form to send new microchip number to db + input validation of correct number format-->
+
+        <p>Documents(upload)</p>
+        <p>Upload icon + vaccine certificate</p><br><!-- button to allow file upload to db + get title and file type of existing Documents in db(clarify on title is it fixed eg vacc cert or will there be other types along with the file type) -->
+
+        <p>Temperament</p>
+        <p>Display temperament seperated by commas + edit functionality & icon</p><br><!-- get temperament from db + send input to db + input validation clarify input(since in onboarding there's fixed 8 inputs so would users be able to select these or would they manually type it out) -->
+
+        <p>Favourite Things</p>
+        <p>Display Favourite Things seperated by commas + edit functionality & icon</p><br><!-- get Favourite Things from db + send input to db + input validation clarify same as Temperament -->
+
+        <p>Dislikes</p>
+        <p>Display Dislikes seperated by commas + edit functionality & icon</p><br><!-- get Dislikes from db + send input to db + input validation clarify same as Temperament -->
+
+        <p>Training Level</p>
+        <p>Display Training Level seperated by commas + edit functionality & icon</p><br><!-- get Training Level from db + send input to db + input validation clarify same as Temperament -->
+
+        <p>Exercise & Lifestyle</p>
+        <p>Display Exercise & Lifestyle seperated by commas + edit functionality & icon</p><br><!-- get Exercise & Lifestyle from db + send input to db + input validation clarify same as Temperament -->
+
+        <p>Looking To Improve</p>
+        <p>Display Looking To Improve seperated by commas + edit functionality & icon</p><br><!-- get Looking To Improve from db + send input to db + input validation clarify inputs taken from above few -->
+
+        <p>Save Settings</p><!-- form submission where everything is saved to the db>
+    </body>
+</html>
