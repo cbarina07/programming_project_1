bcf7925c0ba9945213e1517ba7d66a6af0bc18d5
Yihengggoh <s3882810@student.rmit.edu.au>
2025-03-27 16:27:36 +1100

    new: skeleton code for personal information

---
+++ edit_person.php
@@ -0,0 +1,31 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Edit dog profile</title>
+    </head>
+    <body>
+        <h3>Personal Information</h3> <!--add link to go back to previous page 'settings' page-->
+
+        <p>Name</p>
+        <p>Display profile name + edit functionality & icon</p><br> <!--form(?) to send new name to db + input validation-->
+
+        <p>Email</p>
+        <p>Display email + edit functionality & icon</p><br> <!--form(?) to send new email to db + input validation-->
+
+        <p>Password</p>
+        <p>Display lock icon + masked password + edit functionality & icon</p><br> <!--form(?) to send new name to db + input validation, clarify password policy + storage system (hash/salt etc)-->
+
+        <p>Location</p>
+        <p>Display location icon + location (city, country)</p><br> <!--form(?) to send new location to db + input validation, clarify drop down list of all location or only specific region/only user input-->
+
+        <p>Date of Birth</p>
+        <p>Cake icon + DoB in (mmm dd, yyyy) + edit functionality & icon</p><br><!-- clarify what client would like (user input/clicking on visual calender etc) + input validation-->
+
+        <p>Gender</p>
+        <p>Selection between male/female</p><br><!-- form to get male/female button using radio?-->
+
+        <!-- in edit personal its DoB -> gender but in dog profile its gender -> DoB (is it a design thing or does client want to standardise)--> 
+
+        <p>Save Settings</p><!-- form submission where everything is saved to the db>
+    </body>
+</html>
